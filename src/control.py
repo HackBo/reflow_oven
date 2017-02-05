12e40424b893a00f7f317edb35d4dfc45d56b8dc
Nelson Castillo <nelsoneci@gmail.com>
2017-02-04 21:27:09 -0500

    Doing control. We need to improve the code.

--- src/control.py
+++ src/control.py
@@ -64,6 +64,44 @@ class OvenOne:
             self.set_output(temp < 40)
             time.sleep(1.0)
 
+
+def better_temp(oven):
+    all_read = 0.0
+    for _ in range(5):
+        all_read += oven.read_temp()
+    return all_read / 5.-1
+
+def aim_for (t0, oven, yprev, tprev, ynext, tnext, deltat):
+    slope = (ynext - yprev) / (tnext - tprev)
+    const = ynext - slope * tnext
+    tnow = time.time() - t0
+    while tnow <= tnext:
+      time.sleep(deltat)
+      ynow0 = oven.read_temp()
+      tnow = time.time() - t0
+      guess = slope * (tnow + deltat * 3)  + const
+
+      oven.set_output(ynow0 < guess)
+
+      log = '{} {} {} {}'.format(round(tnow, 2), round(tnext, 2), round(ynow0, 2), round(ynext, 2))
+      print(log)
+      sys.stdout.flush()
+      print('t0 t1 realt goal_t', file=sys.stderr)
+      print(log, file=sys.stderr)
+      sys.stderr.flush()
+
+def read_curve(filename):
+    f = open(filename, 'r')
+    v = []
+    l = f.readline().strip()
+    while len(l):
+      if l.find('#') == -1:
+        x, y = l.rsplit(' ')
+        v.append([float(x), float(y)])
+      l = f.readline().strip()
+    f.close()
+    return v
+
 def main():
     ' Our main funcion. Open Arduino and send pings '
     logging.info('Be safe!')
@@ -79,8 +117,17 @@ def main():
     logging.info('Ping OK!')
     # Start owen loop. With a fixed temperature, just for a test.
     oven = OvenOne(arduino)
-    oven.loop(temp_target=103)
+    oven.select(0)
+    program = read_curve('splinedata')
+    time_before = program[0][0]
+    temp_before = program[0][1]
+    t0 = time.time()
+    for v in program[1:]:
+      aim_for(t0, oven, temp_before, time_before, v[1], v[0], 0.1)
+      time_before = v[0]
+      temp_before = v[1]
+    print >> sys.stderr, "Goal reached. End."
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     main()
