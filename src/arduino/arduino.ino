c09750bc3dae6d8191c6e502a8cbab1de45abbbd
Nelson Castillo <nelsoneci@gmail.com>
2017-02-05 11:37:09 -0500

    Stop oven if temperature goves above 240°.

--- src/arduino/arduino.ino
+++ src/arduino/arduino.ino
@@ -6,6 +6,7 @@ const int kPinPort1 = PD3;
 const int kPinErrorLed = PD4;
 const int kTCS0 = PD6;
 const int kTCS1 = PD7;
+const int kMaxTemp = 240 * 4; // 240°
 
 int current_thermo = 0;
 MAX6675 thermocouple0(SCK, kTCS0, MISO);
@@ -28,12 +29,12 @@ void loop() {
     case 'T':
       temp = current_thermo ? thermocouple1.ReadCelsiusByFour() :
                               thermocouple0.ReadCelsiusByFour();
-      if (temp == -1)
-        goto disconnected;
+      if (temp == -1 || temp > kMaxTemp)
+        goto error;
       sprintf(buffer, "%ld.", temp);
       Serial.print(buffer);
       break;
-    default: disconnected:
+    default: error:
       digitalWrite(kPinPort0, LOW);
       digitalWrite(kPinPort1, LOW);
       while(1) {
