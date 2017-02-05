af498674e1c1b8d646142add4e805dc18caa9550
Nelson Castillo <nelsoneci@gmail.com>
2017-02-05 09:58:37 -0500

    Open right program.

--- src/main.py
+++ src/main.py
@@ -10,13 +10,12 @@ from oven import OvenOne
 
 def main():
     ' Our main funcion. Open Arduino and send pings '
-    logging.info('Be safe!')
     logging.info('Trying to open port %s', sys.argv[1])
     arduino = arduinocmd.Arduino(port=sys.argv[1], baudrate=9600)
     time.sleep(1.0)
     oven = OvenOne(arduino)
     oven.select(0)
-    control = OvenControl(oven, 'splinedata', zone_degrees=10)
+    control = OvenControl(oven, 'oven_program.txt', zone_degrees=10)
     control.follow_curve()
 
 if __name__ == '__main__':
