fb469f2132e58b83b6050955189cb9fef910c9b1
Nelson Castillo <nelsoneci@gmail.com>
2017-02-05 09:45:38 -0500

    Main function ready.

--- src/main.py
+++ src/main.py
@@ -4,27 +4,9 @@ import logging
 import sys
 import time
 
-import oven
-
-def aim_for (t0, oven, yprev, tprev, ynext, tnext, deltat):
-    slope = (ynext - yprev) / (tnext - tprev)
-    const = ynext - slope * tnext
-    tnow = time.time() - t0
-    while tnow <= tnext:
-      time.sleep(deltat)
-      ynow0 = oven.read_temp()
-      tnow = time.time() - t0
-      guess = slope * tnow  + const
-
-      oven.set_output(ynow0 < guess)
-
-      #log = '{} {} {} {}'.format(round(tnow, 2), round(tnext, 2), round(ynow0, 2), round(ynext, 2))
-      log = '{} {}'.format(round(tnow, 2), round(ynow0, 2))
-      print(log)
-      sys.stdout.flush()
-      print('time temp', file=sys.stderr)
-      print(log, file=sys.stderr)
-      sys.stderr.flush()
+import arduinocmd
+from control import OvenControl
+from oven import OvenOne
 
 def main():
     ' Our main funcion. Open Arduino and send pings '
@@ -32,17 +14,10 @@ def main():
     logging.info('Trying to open port %s', sys.argv[1])
     arduino = arduinocmd.Arduino(port=sys.argv[1], baudrate=9600)
     time.sleep(1.0)
-    # Sending ping.
-    oven = oven.OvenOne(arduino)
+    oven = OvenOne(arduino)
     oven.select(0)
-    program = read_curve('splinedata')
-    time_before = program[0][0]
-    temp_before = program[0][1]
-    t0 = time.time()
-    for v in program[1:]:
-      aim_for(t0, oven, temp_before, time_before, v[1], v[0], 0.25)
-      time_before = v[0]
-      temp_before = v[1]
+    control = OvenControl(oven, 'splinedata', zone_degrees=10)
+    control.follow_curve()
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
