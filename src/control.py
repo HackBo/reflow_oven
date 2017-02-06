a784a0dcf97650a192c3d596c80f9a24bdf0bb44
Nelson Castillo <nelsoneci@gmail.com>
2017-02-05 22:19:00 -0500

    Removed unused logging from control.py.
      TODO: Restore later. Not with the HW right now.

--- src/control.py
+++ src/control.py
@@ -2,7 +2,6 @@
 
 import sys
 import time
-import logging
 
 def read_curve(filename):
     ' Read control curve from file '
@@ -60,15 +59,11 @@ class OvenControl:
                 proportion = 1.0
             else:
                 proportion = min(1.0, (error * self.mult) / self.zone)
-            logging.info('error: %s', error)
             print('time:{} temp:{} target:{} error:{} proportion:{}'.format(
                 round(time_in_curve, 2), round(temp_0, 2), round(temp_wanted, 2),
                 round(error, 2), round(proportion, 2)),
                   file=sys.stderr)
             sys.stdout.flush()
-            #logging.info('error * mult: %s', error * self.mult)
-            #logging.info('error * mult / zone: %s', (error * self.mult) / self.zone)
-            #logging.info('proportion %s', proportion)
             # Only turn on if we need to do some control.
             self.oven.set_output(proportion > 0.0)
             # Time reading from the thermocouple.
@@ -78,8 +73,6 @@ class OvenControl:
             # How long do we need to be on and off?
             time_need_on = self.time_window * proportion - time_thermo
             time_need_off = self.time_window - time_need_on
-            #logging.info('time_on: %s', time_need_on)
-            #logging.info('time_off: %s', time_need_off)
             time.sleep(max(0, time_need_on))
             self.oven.set_output(proportion >= 1.0)
             time.sleep(max(0, time_need_off))
